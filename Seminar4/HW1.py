18efef8bec05c95d51443235602f18be70e7d591
cudestnik <cash2000@bk.ru>
2023-05-31 05:27:21 +0300

    ДЗ 1 решил

---
+++ Seminar4/HW1.py
@@ -0,0 +1,26 @@
+# Даны два неупорядоченных набора целых чисел (может быть, с повторениями).
+# Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
+# Пользователь вводит 2 числа.
+# n - кол-во элементов первого набора.
+# m - кол-во элементов второго набора.
+# Значения генерируются случайным образом.
+
+
+def create_array(n, mn, mx):
+    import random
+    array = [random.randint(mn, mx) for _ in range(n)]
+    return array
+
+
+n = int(input('Кол-во элементов первого набора? -> '))
+m = int(input('Кол-во элементов второго набора? -> '))
+listing_1 = create_array(n, 2, 13)
+listing_2 = create_array(m, 2, 13)
+print(listing_1)
+print(listing_2)
+
+set_1 = set(listing_1)
+set_2 = set(listing_2)
+
+result = set_1 & set_2
+print('Пересечение множеств - ', sorted(result), '\n')
